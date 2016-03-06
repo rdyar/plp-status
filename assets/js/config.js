7f33b4a24fdd7347c81f023a56c29652a4efdf4c
ron dyar <ron@fromex.net>
2016-03-06 15:31:59 -0800

    initializing

---
+++ assets/js/config.js
@@ -0,0 +1,12 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm776747773-45ae8d6bb5c12bf77dba6ab3','m776755357-7e8eed7ae5962e3cceeb50c7','m776747768-3d375a0ca9da14318bfd2dc9'
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
