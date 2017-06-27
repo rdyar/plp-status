89c668cc3e92a1d1bc9198b8ac8b8731a49e768e
Ron Dyar <ron@fromex.net>
2017-06-26 17:15:31 -0700

    added rtp backup monitor

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm776747773-45ae8d6bb5c12bf77dba6ab3','m776755357-7e8eed7ae5962e3cceeb50c7','m776747768-3d375a0ca9da14318bfd2dc9','m778231061-4a46cc77dcada25963803ad2'
+	'm776747773-45ae8d6bb5c12bf77dba6ab3','m776755357-7e8eed7ae5962e3cceeb50c7','m776747768-3d375a0ca9da14318bfd2dc9','m778231061-4a46cc77dcada25963803ad2','m779148126-cd71a90f5bc5b9b946e2341f'
 ];
 
 // refresh interval (in seconds)
